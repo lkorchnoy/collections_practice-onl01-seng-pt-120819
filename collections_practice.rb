73b57218f0cbca4c750a86f759e1701327bf3ff4
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-19 08:58:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(int)
+  int.sort do |a, b|
+    a <=> b
+  end
+  
+  def sort_array_desc(int)
+    int.sort do |b, a|
+      b <=> a
+    end
+    
+    def sort_array_char_count(string)
+      string.sort do |a.length, b.length|
+    a.length <=> b.length
+  end
+      
+      def swap_elements(array, index, destination_index)
+        array.swap[1][2] to [2][1]
+      end
+      
+      def reverse_array(int)
+        int.sort do |a, b reverse|
+           a <=> b
+  end
+  
+  def kesha_maker(strings)
+    i=0
+    new_string = []
+    while i < strings.length do
+      strings.each do |element|
+        new_string << element[2] = $
+      i += 1
+end
+  return new_string
+end
+ 
+ def find_a(string)
+   string.collect do |word|
+     word.start_with?("a")
+   end
+   
+     def sum_array(n)
+       n.inject { |sum, n| sum + n }
+   end
+   
+   def add_s(arr)
+     arr = ["hand","feet", "knee", "table]
+     [1, 3, 4].each_with_index.collect { |element, index| }
+     ellement << add_s
+   end
+ end
+     
 
+      
+    
+  
